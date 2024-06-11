ad545a3cb3e4e2cef84f2a4bff8dd607857fb10c
tylerlarson919 <65380343+tylerlarson919@users.noreply.github.com>
2024-06-10 21:54:34 -0400

    Update audio-player.js

--- audio-player.js
+++ audio-player.js
@@ -1,14 +1,5 @@
 (function () {
     'use strict';
-    try {
-        if (typeof document !== 'undefined') {
-            var t = document.createElement('style');
-            t.appendChild(document.createTextNode(".wrapper{display:flex;flex-direction:column;align-items:stretch;height:7rem;justify-content:space-around}.wrapper .input{background-color:#f0f8ff}.wrapper input{width:100%;padding:10px;border:1px solid #e4e4e4;border-radius:3px;outline:none;font-size:14px}"));
-            document.head.appendChild(t);
-        }
-    } catch (t) {
-        console.error("vite-plugin-css-injected-by-js", t);
-    }
 
     const icon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-width="2" d="M10 10.5606V13.4394C10 14.4777 11.1572 15.0971 12.0211 14.5211L14.1803 13.0817C14.9536 12.5661 14.9503 11.4317 14.18 10.9181L12.0214 9.47907C11.1591 8.9042 10 9.5203 10 10.5606Z"/><rect width="14" height="14" x="5" y="5" stroke="currentColor" stroke-width="2" rx="4"/></svg>';
 
