ef4178ec85615cd9445f38dd46d179f7b5b7967e
tylerlarson919 <65380343+tylerlarson919@users.noreply.github.com>
2024-06-18 23:00:53 -0400

    Update playerOne.js

--- LiFERPG Assets/Enemys_Scripts/playerOne.js
+++ LiFERPG Assets/Enemys_Scripts/playerOne.js
@@ -1,29 +1,35 @@
 const playerOne = {
-  idle: () => {
+  idle: function() {
+    const playerImage = document.getElementById('player-image');
     playerImage.src = 'https://github.com/tylerlarson919/stuff/blob/main/LiFERPG%20Assets/animationSprites/Player_samuri/player_samurri_idol.gif?raw=true';
     // Loop the idol animation
-    playerImage.addEventListener('animationiteration', () => {
+    playerImage.addEventListener('animationiteration', function() {
       playerImage.src = 'https://github.com/tylerlarson919/stuff/blob/main/LiFERPG%20Assets/animationSprites/Player_samuri/player_samurri_idol.gif?raw=true';
     });
   },
-  attack: () => {
+  attack: function() {
+    const playerImage = document.getElementById('player-image');
     playerImage.src = 'https://github.com/tylerlarson919/stuff/blob/main/LiFERPG%20Assets/animationSprites/Player_samuri/player_samurri_charge.gif?raw=true';
-    setTimeout(() => {
+    setTimeout(function() {
       playerImage.src = 'https://github.com/tylerlarson919/stuff/blob/main/LiFERPG%20Assets/animationSprites/Player_samuri/player_samurri_Attack.gif?raw=true';
       // Loop the idol animation after attacking
-      playerImage.addEventListener('animationiteration', () => {
+      playerImage.addEventListener('animationiteration', function() {
         playerImage.src = 'https://github.com/tylerlarson919/stuff/blob/main/LiFERPG%20Assets/animationSprites/Player_samuri/player_samurri_idol.gif?raw=true';
       });
     }, 1000);
   },
-  Death: () => {
+  death: function() {
+    const playerImage = document.getElementById('player-image');
     playerImage.src = 'https://github.com/tylerlarson919/stuff/blob/main/LiFERPG%20Assets/animationSprites/Player_samuri/player_samurri_takeDamage.gif?raw=true';
-    setTimeout(() => {
+    setTimeout(function() {
       playerImage.src = 'https://github.com/tylerlarson919/stuff/blob/main/LiFERPG%20Assets/animationSprites/Player_samuri/player_samurri_death.gif?raw=true';
       // Blank image after playing death once
-      setTimeout(() => {
+      setTimeout(function() {
         playerImage.src = ''; // Set to blank image
       }, 1000);
     }, 1000);
   }
 };
+
+// Export playerOne object to make it accessible globally
+window.playerOne = playerOne;
